1829c6e8c8f0762c6bcae1c736698a66295b1efc
coderDaiyan <daiyanabdallah@gmail.com>
2021-02-01 23:02:22 +0600

    truthy vs falsy

---
+++ true-false.js
@@ -0,0 +1,12 @@
+// Truthy Values
+// Any Numbers, Empty Array [], Empty Object {}, Whitespace ' '
+
+// Falsy Values
+// Null, Undefined, 0, '', Nan
+
+let num = {}
+if(num){
+    console.log("This is true");
+} else {
+    console.log("This is false");
+}
