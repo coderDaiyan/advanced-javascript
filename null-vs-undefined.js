7a3f011e0db5996c0d4aa9e5cae51dabff192533
coderDaiyan <daiyanabdallah@gmail.com>
2021-02-02 00:17:09 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+// let test;
+
+// function sum(x, y){
+//     console.log(x + y);
+//     return
+// }
+
+// function add(num1, num2){
+//     return num1, num2;
+// }
+
+// const student = {name: "Javed Karim", age: 12}
+// console.log(student.phone);
+
+// let fun = undefined      // Not recommended
+// console.log(fun);
+
+// let ages = [12, 45, 35]
+// console.log(ages[5]);
+
+// const test = null
+// console.log(test);
