8d2259e77fa71664fb6d328e6af83c1a7b89ef19
coderDaiyan <daiyanabdallah@gmail.com>
2021-02-02 22:34:42 +0600

    i have learned so many things

---
+++ array-split.js
@@ -0,0 +1,9 @@
+const nums = [2, 3, 4, 5, 6, 7, 8, 9];
+// const part = nums.slice(2, 6)
+
+// const test = nums
+// test.splice(2, 5, "Hi")
+// console.log(test);
+
+const together = nums.join(" ami ")
+console.log("ami " + together);
---
+++ scope.js
@@ -0,0 +1,11 @@
+const bonus = 10
+function sum(first, second){
+    const result = first + second + bonus
+    // console.log(bonus);
+    if(result > 10)
+    return result
+}
+
+const output = sum(25, 25)
+console.log(bonus);
+console.log(output);
